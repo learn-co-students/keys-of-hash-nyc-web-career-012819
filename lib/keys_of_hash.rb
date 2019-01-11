908ce659251e1ab63f3af6e9bd3e3c2aebedbae8
github username <github email address>
2019-01-10 19:00:25 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    args=[]
+    args.push(*arguments)
+    self.map do |x,y|
+      if args.include?(y)
+        array.push(x)
+        end
+      end
+    return array
   end
 end
