cf448be5a790627f8e49f1ef577fe892dfdacf55
github username <github email address>
2019-01-19 17:36:31 -0500

    monkey patched

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+    i = 0
+    while i < arguments.length
+      self.each {|attributes, value|
+        if value == arguments[i]
+          array << attributes
+        end
+      }
+      i += 1
+    end
+      #binding.pry
+    array
   end
 end
